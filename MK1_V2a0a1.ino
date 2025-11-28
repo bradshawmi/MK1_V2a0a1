ea314b0e1d92d1cf87176d20d629b780900aaf8c
bradshawmi <bradshawmi@gmail.com>
2025-11-28 01:04:05 -0700

    Update MK1_V2a0a1.ino

--- MK1_V2a0a1.ino
+++ MK1_V2a0a1.ino
@@ -19,7 +19,7 @@ static inline void auroraUpdate(uint8_t z, uint16_t speed);
 static inline CRGB auroraSample(uint8_t z, uint16_t iGlobal, uint8_t intensity);
 static inline uint8_t auroraHolesMask(uint8_t z, uint16_t iGlobal);
 
-static constexpr char BUILD_TAG[] = "v2a0d0";
+static constexpr char BUILD_TAG[] = "v2a0d1";
 
 enum DFPhase : uint8_t;
 struct DFState;
